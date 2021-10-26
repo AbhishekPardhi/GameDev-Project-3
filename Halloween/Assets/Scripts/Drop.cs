b03b21b791a638289e7862a1026700c8eb5f7f23
mabhishek20 <82402416+mabhishek20@users.noreply.github.com>
2021-10-26 20:26:09 +0530

    object detection

--- Halloween/Assets/Scripts/Drop.cs
+++ Halloween/Assets/Scripts/Drop.cs
@@ -5,6 +5,9 @@ using UnityEngine.UI;
 
 public class Drop : MonoBehaviour
 {
+    RaycastHit hit; 
+    float Reach = 2.0f;
+    bool RayHit;
     Camera cam;
     Vector3 pos = new Vector3(1, 1, 0);
 
@@ -19,5 +22,19 @@ public class Drop : MonoBehaviour
         Ray ray = cam.ScreenPointToRay(pos);
         Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
     }
-    
+    void ObjectDetection()
+    {
+        var fwd = transform.TransformDirection (Vector3.forward);
+        Debug.DrawRay(transform.position, fwd * Reach, Color.yellow);
+         
+         if (Physics.Raycast (transform.position, fwd, Reach)&&hit.transform.gameObject.tag == "Objects") 
+         {
+             RayHit = true;
+             Debug.Log("You can pick up the object");
+         }
+         else 
+         {
+            RayHit = false;
+         }
+     }
 }
